4e8cede14b605f6aa21c1cd8da999ef48c3aae6a
Nick Arner <nicholasarner@gmail.com>
2023-06-06 14:21:55 -0700

    Revert "package"
    This reverts commit 6cbee01bbe93226537e8f4af228ae0e06e1d84cb.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 let package = Package(
     name: "roboflow-swift",
     defaultLocalization: "en",
-
+    platforms: [
+        .iOS(.v16),
+    ],
     products: [
         .library(
             name: "Roboflow",
